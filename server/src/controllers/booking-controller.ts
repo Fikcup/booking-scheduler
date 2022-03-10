41781139d0d0404c7060f2a660238dbbae88de37
Fikcup <rhys.n.wright@gmail.com>
2022-03-10 14:06:43 -0800

    feat : joined address table to booking query

--- server/src/controllers/booking-controller.ts
+++ server/src/controllers/booking-controller.ts
@@ -6,13 +6,19 @@ import AddressDto from '../dtos/address.dto';
 
 const bookingController = {
     async getBookings(req: Request, res: Response) {
-        const bookings = await getConnection()
-            .createQueryBuilder()
-            .select('booking')
-            .from(Booking, 'booking')
-            .getMany();
-
-        res.status(200).send(bookings);
+        try {
+            const bookings = await getConnection()
+                .createQueryBuilder()
+                .select('booking')
+                .from(Booking, 'booking')
+                .innerJoinAndSelect('booking.address', 'address')
+                .getMany();
+    
+            res.status(200).send(bookings);
+        } catch (err: any) {
+            console.error(err);
+            res.status(500).send(err.message);
+        }
     },
     async createBooking(req: Request, res: Response) {
         // TODO: create safeguard for appointment time double booking
@@ -24,47 +30,48 @@ const bookingController = {
             zip: req.body.zip
         }
 
-        if (req.body.apt) {
-            addressObj.apt = req.body.apt;
+        try {
+            const address = await getConnection()
+                .createQueryBuilder()
+                .insert()
+                .into(Address)
+                .values([addressObj])
+                .execute();
+
+            let type;
+            if (req.body.type === 'housekeeping') {
+                type = BookingType.HOUSEKEEPING;
+            }
+    
+            if (req.body.type === 'dog walk') {
+                type = BookingType.DOGWALK;
+            }
+    
+            const bookingObj = {
+                name: req.body.name,
+                email: req.body.email,
+                type: type,
+                date: req.body.date,
+                time: req.body.time,
+                address: address.raw.insertId
+            };
+    
+            const booking = await getConnection()
+                .createQueryBuilder()
+                .insert()
+                .into(Booking)
+                .values([bookingObj])
+                .execute();
+    
+            if (!address || !booking) {
+                res.status(500).send({ message: 'Form data invalid' });
+            }
+    
+            res.status(200).send(bookingObj);
+        } catch (err: any) { 
+            console.error(err);
+            res.status(500).send(err.message); 
         }
-
-        const address = await getConnection()
-            .createQueryBuilder()
-            .insert()
-            .into(Address)
-            .values([addressObj])
-            .execute();
-
-        let type;
-        if (req.body.type === 'housekeeping') {
-            type = BookingType.HOUSEKEEPING;
-        }
-
-        if (req.body.type === 'dog walk') {
-            type = BookingType.DOGWALK;
-        }
-
-        const bookingObj = {
-            name: req.body.name,
-            email: req.body.email,
-            type: type,
-            date: req.body.date,
-            time: req.body.time,
-            address: address.raw.insertId
-        };
-
-        const booking = await getConnection()
-            .createQueryBuilder()
-            .insert()
-            .into(Booking)
-            .values([bookingObj])
-            .execute();
-
-        if (!address || !booking) {
-            res.status(500).send({ message: 'Form data invalid' });
-        }
-
-        res.status(200).send(bookingObj);
     }
 }
 
