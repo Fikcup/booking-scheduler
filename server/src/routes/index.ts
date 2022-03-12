dadf291496d99adf8d7a313ae864ae2094c868be
Fikcup <rhys.n.wright@gmail.com>
2022-03-12 13:23:23 -0800

    fix : wrong route

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -2,6 +2,8 @@ import express, { Request, Response } from 'express';
 import apiRoutes from './api';
 const router = express.Router();
 
+router.use('/');
+
 router.use('/api', apiRoutes);
 
 router.use((req: Request, res: Response) => {
