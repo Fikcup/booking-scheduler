452ffbb004c16c4826d4c3f7be9848a4efd4a2d8
Fikcup <rhys.n.wright@gmail.com>
2022-03-12 16:33:34 -0800

    fix : server routing

--- server/src/server.ts
+++ server/src/server.ts
@@ -13,15 +13,15 @@ app.use(express.urlencoded({
     extended: true
 }));
 app.use(cors());
+app.use(routes);
 
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, '../../client/build')));
 }
   
-app.get('*', (req, res) => {
+app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../../client/build/index.html'));
 });
-app.use(routes);
 
 
 connection
