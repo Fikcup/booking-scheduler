c5babcd6971e7bf45e0fe288503f5e0326768f97
Fikcup <rhys.n.wright@gmail.com>
2022-03-13 20:12:25 -0700

    fix : date display

--- client/src/utils/DateFormat.ts
+++ client/src/utils/DateFormat.ts
@@ -1,9 +1,11 @@
+import { parseISO } from 'date-fns';
+
 const dateFormat = (date: string) => {
-    const format = new Date(date).toLocaleString('en-US', {
+    const format = parseISO(new Date(date).toLocaleString('en-US', {
         month: 'long',
         day: 'numeric',
         year: 'numeric'
-    });
+    }));
 
     return format;
 }
