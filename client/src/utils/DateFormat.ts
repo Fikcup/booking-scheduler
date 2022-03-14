e22d0de6d1df6e0c7cf44b38b720fcd76129e886
Fikcup <rhys.n.wright@gmail.com>
2022-03-13 20:17:05 -0700

    feat : formatted date to timezone

--- client/src/utils/DateFormat.ts
+++ client/src/utils/DateFormat.ts
@@ -1,11 +1,11 @@
 import { parseISO } from 'date-fns';
 
 const dateFormat = (date: string) => {
-    const format = parseISO(new Date(date).toLocaleString('en-US', {
+    const format = parseISO(date).toLocaleString('en-US', {
         month: 'long',
         day: 'numeric',
         year: 'numeric'
-    }));
+    });
 
     return format;
 }
